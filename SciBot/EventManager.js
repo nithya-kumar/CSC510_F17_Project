7cc8e7e67570f03033a976bc595fd67d06773516
ssharma25 <ssharm25@ncsu.edu>
2017-10-22 14:07:23 -0400

    Updating EventMAnager and Parser

---
+++ SciBot/EventManager.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var fs = require("fs");
+var Promise = require('bluebird');
+var parse = require('parse-link-header');
+
+
+function createPing(message, user, day, date)
+{
+	var options = {
+		url: https://slack.com/api/reminders.add,
+		method: 'POST',
+		headers: {
+			
+		}
+	};
+}
