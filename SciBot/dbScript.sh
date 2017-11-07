64e6488a0f661d4e72a1847724eaebf65fba2c5f
Anshul Chandra <achand13@ncsu.edu>
2017-11-07 14:58:44 -0500

    updated columns in db script

--- SciBot/dbScript.sh
+++ SciBot/dbScript.sh
@@ -1,8 +1,18 @@
 #Create table for tracking status
-CREATE TABLE scrum_status(status_username varchar references users(username), 
-status_today varchar, status_yesterday varchar, status_obstacles varchar, 
-status_date date, status_time time, primary key(status_username,status_date));
+create table status (
+  username varchar(25) references users(username),
+  status_today text,
+  status_yesterday text,
+  status_obstacles text,
+  status_date date,
+  status_time time
+);
 
 #Create table to track user, user roles, configured pings
-CREATE TABLE user(username varchar primary key not null, privilege varchar, ping_timestamp timestamp);
+create table users (
+	username  varchar(25) primary key,
+	Full_name varchar(100) not null,
+	is_admin boolean not null,
+	ping_timestamp time
+);
 
